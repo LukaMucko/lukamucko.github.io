d5d497f4455b29783a6ef0b64722fb36c91bee67
Luka Mucko <luka.mucko@gmail.com>
2026-02-06 00:22:56 +0100

    Add GitHub Pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://lukamucko.github.io',
     integrations: [mdx(), react()],
     markdown: {
         shikiConfig: {
